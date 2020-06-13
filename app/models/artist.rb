03fa6b9d18a2b80e81e2b315896f4d95c961c28d
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-06-13 23:29:18 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -9,11 +9,11 @@ class Artist < ActiveRecord::Base
 
   def song_count
     #return the number of songs associated with the artist
-    self.songs.first.genre
+    self.songs.count
   end
 
   def genre_count
     #return the number of genres associated with the artist
-    self.songs.genre.count
+    self.genre.count
   end
 end
